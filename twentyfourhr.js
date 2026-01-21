7bb4f16a082e373dbb20a96612b5d5ae610d2b4e
Faith Njoroge <njorogefaith6937@gmail.com>
2026-01-21 22:21:30 +0300

    Add function to display current date and time

---
+++ twentyfourhr.js
@@ -0,0 +1,17 @@
+function showDateTime() {
+    const now = new Date();
+
+    const formatted = now.toLocaleString("en-GB", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false
+    });
+
+    console.log(formatted);
+}
+
+setInterval(showDateTime, 1000);
